a1b4e106186a48aff77f964eb546a52a2dba446f
Vinayram467 <vinayram630@gmail.com>
2025-08-11 17:52:56 +0530

    cursor edit

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,44 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+// Send GA4 page_view on SPA navigations
+function initRouteChangeTracking() {
+  if (typeof window === 'undefined') return;
+  const sendPageView = () => {
+    // @ts-ignore
+    if (window.gtag) {
+      // @ts-ignore
+      window.gtag('event', 'page_view', {
+        page_location: window.location.href,
+        page_path: window.location.pathname + window.location.search,
+        page_title: document.title,
+      });
+    }
+  };
+
+  // Initial
+  sendPageView();
+
+  // Track pushState/replaceState and popstate
+  const pushState = history.pushState;
+  history.pushState = function (...args) {
+    // @ts-ignore
+    const ret = pushState.apply(this, args as any);
+    sendPageView();
+    return ret;
+  } as any;
+
+  const replaceState = history.replaceState;
+  history.replaceState = function (...args) {
+    // @ts-ignore
+    const ret = replaceState.apply(this, args as any);
+    sendPageView();
+    return ret;
+  } as any;
+
+  window.addEventListener('popstate', sendPageView);
+}
+
+initRouteChangeTracking();
+
 createRoot(document.getElementById("root")!).render(<App />);
